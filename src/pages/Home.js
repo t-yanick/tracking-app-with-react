631c7fc1a7a6a1d63e84438cc4bf27f7c4f9609c
Yanick <tyankee15@gmail.com>
2021-09-05 04:27:22 -0300

    Create Home component as homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Home = () => (
+  <div>
+    <h1 className="heading">Home</h1>
+    <div className="content">
+      <h2 className="home__heading">
+        Welcome to Track it App
+      </h2>
+    </div>
+  </div>
+);
+
+export default Home;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,21 +9,22 @@ const LoginPage = ({
   const [errors, setErrors] = useState([]);
   const [msg, setMsg] = useState('');
 
-};
 
-const handleSubmit = (username, password) => {
 
-};
+  const handleSubmit = (username, password) => {
 
-return (
-  <div>
-    <h1 className="heading">Login</h1>
-    <div className="content">
-      Error Message
-      <UsersForm handleSubmit={handleSubmit} btnName="Login" />
-      <Link to="/" className="btn">Go back to Home</Link>
+  };
+
+  return (
+    <div>
+      <h1 className="heading">Login</h1>
+      <div className="content">
+        Error Message
+        <UsersForm handleSubmit={handleSubmit} btnName="Login" />
+        <Link to="/" className="btn">Go back to Home</Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginPage;
