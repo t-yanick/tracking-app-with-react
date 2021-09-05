cb749253644a311f8eb11700b1032c774ee576f9
Yanick <tyankee15@gmail.com>
2021-09-05 04:54:47 -0300

    Add npm react-icoms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BsBarChartFill } from 'react-icons/bs';
+import { FaCheck } from 'react-icons/fa';
+import { GiPieChart } from 'react-icons/gi';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
 
 const Footer = () => (
   <footer className="footer">
     <nav className="footer__nav">
       <NavLink to="" className="footer__nav__item">
-        chart fill
+        <BsBarChartFill />
+        <span className="footer__nav__item__text">Add Track</span>
       </NavLink>
       <NavLink to="" className="footer__nav__item">
-        check
+        <FaCheck />
+        <span className="footer__nav__item__text">Track List</span>
       </NavLink>
       <NavLink to="" className="footer__nav__item">
-        pie chart
+        <GiPieChart />
+        <span className="footer__nav__item__text">Your Progress</span>
       </NavLink>
       <NavLink to="" className="footer__nav__item">
-        horizontal rounded
+        <BiDotsHorizontalRounded />
+        <span className="footer__nav__item__text">More</span>
       </NavLink>
     </nav>
   </footer>
