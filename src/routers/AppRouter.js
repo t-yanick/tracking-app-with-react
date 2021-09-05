9663a20d50478fbc38e7840b775b61f06f56aa18
Yanick <tyankee15@gmail.com>
2021-09-05 04:06:49 -0300

    Create App routing component with basic routing to Home page

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Home from '../pages/Home';
+import LoginPage from '../pages/LoginPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div className="whole-container">
+      Login Status
+      Header
+
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginPage} />
+        </Switch>
+      </div>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
