b46b00b11226f6d7c3b9aa484ec4f08f1159ebaf
Yanick <tyankee15@gmail.com>
2021-09-07 11:17:45 -0300

    Fix node-sass bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import './assets/styles/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
