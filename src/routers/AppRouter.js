6600c0a3cbf4880856266439a14e598baea70886
Yanick <tyankee15@gmail.com>
2021-09-18 13:32:41 -0300

    Update routing and refactor routing to accept history as props

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,26 +7,45 @@ import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import AddTrack from '../pages/AddTrack';
 import EditTrack from '../pages/EditTrack';
+import TrackList from '../pages/TrackList';
+import TrackItems from '../pages/TrackItems';
+import Progress from '../pages/Progress';
+import More from '../pages/More';
+import NotFound from '../pages/NotFound';
 import Header from '../components/Header';
+import LoginStatus from '../components/LoginStatus';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div className="whole-container">
-      Login Status
+      <LoginStatus />
       <Header />
 
       <div className="container">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/signup" component={SignupPage} />
-          <Route exact path="/track/create" component={AddTrack} />
+          <Route exact path="/login" render={(props) => (<LoginPage history={props.history} />)} />
+          <Route exact path="/signup" render={(props) => (<SignUpPage history={props.history} />)} />
+          <Route exact path="/track/create" render={(props) => (<AddTrack history={props.history} />)} />
+          <Route exact path="/tracks/:id" component={TrackItems} />
           <Route exact path="/track/:id/edit" component={EditTrack} />
+          <Route exact path="/tracks" component={TrackList} />
+          <Route exact path="/progress" component={Progress} />
+          <Route exact path="/more" component={More} />
+          <Route component={NotFound} />
         </Switch>
       </div>
       <Footer />
     </div>
   </BrowserRouter>
 );
+
+AppRouter.propTypes = {
+  history: PropTypes.instanceOf(Object),
+};
+
+AppRouter.propTypes = {
+  history: null,
+};
 
 export default AppRouter;
