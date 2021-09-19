f33b92ba8ed89588edcec6eb68d63c8ffdcc197e
Yanick <tyankee15@gmail.com>
2021-09-19 15:02:07 -0300

    Add reducers for respective actions

---
+++ src/reducers/items.js
@@ -0,0 +1,12 @@
+const defaultItemsState = [];
+
+const ItemsReducer = (state = defaultItemsState, action) => {
+  switch (action.type) {
+    case 'ADD_ITEMS':
+      return action.items;
+    default:
+      return state;
+  }
+};
+
+export default ItemsReducer;
