dd81e4353d57a965d6dc80c7a929ace703462d20
Yanick <tyankee15@gmail.com>
2021-09-23 14:55:30 -0300

    Add useEfect in TrackItemList component to check set date sign

--- src/components/TrackListItem.js
+++ src/components/TrackListItem.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Chart from 'react-google-charts';
 import { GiTrophyCup } from 'react-icons/gi';
 import { BiChevronRight } from 'react-icons/bi';
+import { moment } from '../helpers/api';
 import calcAchieveTotalRate from '../helpers/calcAchieveTotalRate';
 
-const TrackListItem = ({ milSec, sameDateTrack, itemNum }) => {
+const TrackListItem = ({ milSec, sameDateTracks, itemNum }) => {
   const [dateSign, setDateSign] = useState('');
+
+  const checkDateSign = () => {
+    const targetDay = moment(milSec);
+
+    if (targetDay.isSame(moment(), 'day')) {
+      setDateSign('today');
+    } else if (targetDay.isSame(moment().subtract(1, 'days'), 'day')) {
+      setDateSign('yesterday');
+    } else if (targetDay.isSameOrBefore(moment().subtract(7, 'days'))) {
+      setDateSign('lastweek')
+    }
+  };
+
+  const setBeforeSign = () => {
+    const beforeArray = Array.from(document.querySelectorAll('.lastweek'));
+    if (beforeArray[0] === undefined || !beforeArray[0].classList) return;
+    beforeArray[0].classList.add('first');
+    beforeArray[0].firstElementChild.textContent = 'before last week';
+  };
+
+  useEffect(() => {
+    checkDateSign();
+    setTimeout(() => {
+      setBeforeSign();
+    }, 1000);
+  }, []);
 
   const totalRate = calcAchieveTotalRate(sameDateTracks, itemNum) || 0;
   const rateForChart = totalRate >= 100 ? 100 : totalRate;
