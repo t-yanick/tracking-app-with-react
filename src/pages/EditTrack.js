46d6e48d934bb27fd2ba55e6df48898c49bb25de
Yanick <tyankee15@gmail.com>
2021-09-23 17:47:16 -0300

    Add functions to update track and add new track

--- src/pages/EditTrack.js
+++ src/pages/EditTrack.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TrackForm from '../components/TrackForm';
+import { updateTrack, addNewTrack } from '../helpers/restTracks';
+import { getItems } from '../helpers/restItems';
+import addItems from '../actions/items';
+import getItemTitles from '../helpers/getItemTitles';
 
 const EditTrack = ({
   loginUser, history, items, addItems, sameDateTracks, targetDate,
@@ -9,11 +14,63 @@ const EditTrack = ({
   const [error, setError] = useState('');
   const [msg, setMsg] = useState('');
 
-  const handleSubmit = () => {
-    Object.keys(state).forEach((key) => {
-
-    })
+  const runGetItems = async () => {
+    try {
+      setError('');
+      const response = await getItems();
+      if (response.length > 0) {
+        addItems(response);
+      } else {
+        setError('No Items')
+      }
+    } catch {
+      setError('Unable to fetch the item data');
+    }
   };
+
+  useEffect(() => {
+    runGetItems();
+  }, []);
+
+  const runUpdateTrack = async (trackId, result, itemId, date) => {
+    try {
+      setError('');
+      await updateTrack(trackId, result, itemId, date);
+    } catch {
+      setError('Unable to update track');
+    }
+  };
+
+  const runAddNewTrack = async (result, itemId, date) => {
+    try {
+      setError('');
+      await addNewTrack(result, itemId, date);
+    } catch {
+      setError('Unable to create new track');
+    }
+  };
+
+  const handleSubmit = (StrDate, state) => {
+    Object.keys(state).forEach((key) => {
+      const submitItemId = Number(key);
+      const submitItemValue = state[key]
+
+      const sameItemTrack = sameDateTracks.find((track) => track.item_id === submitItemId);
+      if (sameItemTrack && submitItemValue) {
+        runUpdateTrack(sameItemTrack.id, submitItemValue, submitItemId, StrDate);
+      } else if (submitItemValue) {
+        runAddNewTrack(submitItemValue, submitItemId, StrDate);
+      }
+    });
+    if (!error) {
+      setMsg('Updating track now...');
+      setTimeout(() => {
+        history.push(`/tracks/${StrDate}`);
+      }, 800);
+    }
+  };
+
+  const itemTitles = getItemTitles(items, sameDateTracks);
 
   return (loginUser ? (
     <div className="add-track">
@@ -27,6 +84,17 @@ const EditTrack = ({
     </div>
   ) : <Redirect to="/" />);
 };
+
+const mapStateToProps = (state, props) => ({
+  sameDateTracks: state.tracks.filter((track) => track.date === props.match.params.id),
+  items: state.items,
+  loginUser: state.user.logIn,
+  targetDate: Number(props.match.params.id),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addItems: (items) => dispatch(addItems(items)),
+});
 
 EditTrack.propTypes = {
   items: PropTypes.instanceOf(Array),
@@ -45,4 +113,4 @@ EditTrack.defaultProps = {
   targetDate: null,
 };
 
-export default EditTrack;
+export default connect(mapStateToProps, mapDispatchToProps)(EditTrack);
