f0fce9eace2191f77d5ed9ff8981dd535f4c6a76
Yanick <tyankee15@gmail.com>
2021-09-25 03:53:04 -0300

    Add functions to get items and tracks in Progress.js and map to state and dispatch

--- src/pages/Progress.js
+++ src/pages/Progress.js
@@ -1,13 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Chart from 'react-google-charts';
 import pluralize from 'pluralize';
+import getAvgRate from '../helpers/getAvgRate';
+import getItemTotalResult from '../helpers/getItemTotalResult';
+import { getTracks } from '../helpers/restTracks';
+import { addTracks } from '../actions/tracks';
+import { addTrackDates } from '../actions/trackDates';
+import { getItems } from '../helpers/restItems';
+import addItems from '../actions/items';
 
 const Progress = ({
   addTracks, loginUser, addTrackDates, trackDates, tracks, items, addItems
 }) => {
   const [error, setError] = useState('');
+
+  const runGetItems = async () => {
+    try {
+      const response = await getItems();
+      if (response.length > 0) {
+        setError('');
+        addItems(response);
+      } else {
+        setError('No Items')
+      }
+    } catch {
+      setError('Unable to fetch the item data');
+    }
+  };
+
+  const runGetTracks = async () => {
+    try {
+      const response = await getTracks();
+      if (response) {
+        setError('');
+        addTracks(response.records);
+        addTrackDates(response.record_dates);
+      } else {
+        setError('No Tracks')
+      }
+    } catch {
+      setError('Unable to fetch data');
+    }
+  };
+
+  useEffect(() => {
+    if (loginUser) {
+      runGetTracks();
+      runGetItems();
+    }
+  }, []);
 
   const arMonth = getAvgRate(trackDates, tracks, 'THIS_MONTH') || 0;
   const percentForChart = arMonth >= 100 ? 100 : arMonth;
@@ -23,7 +67,9 @@ const Progress = ({
           </div>
           <div className="progress__chart__text">
             <span className="txt">You achieved</span>
-            <span className="num">{`${arMonth}`}</span>
+            <span className="num">
+              {`${arMonth}`}
+            </span>
             <span className="txt">on average</span>
           </div>
           <div className="progress__chart__container">
@@ -79,7 +125,7 @@ const Progress = ({
                   {item.title}
                 </div>
                 <div className="progress__item__num">
-                  formattedItemTotal
+                  {formattedItemTotal}
                 </div>
                 <div className="progress__item__unit">
                   {pluralize(item.unit, itemTotalResult)}
@@ -94,12 +140,25 @@ const Progress = ({
   ) : <Redirect to="/" />
 };
 
+const mapStateToProps = (state) => ({
+  items: state.items,
+  tracks: state.tracks,
+  trackDates: state.trackDates,
+  loginUser: state.user.logIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addItems: (items) => dispatch(addItems(items)),
+  addTracks: (tracks) => dispatch(addTracks(tracks)),
+  addTrackDates: (trackDates) => dispatch(addTrackDates(trackDates)),
+});
+
 Progress.propTypes = {
   addTracks: PropTypes.func,
   addTrackDates: PropTypes.func,
   loginUser: PropTypes.bool.isRequired,
   items: PropTypes.instanceOf(Array),
-  tracks: PropTypes.instanceOf(arrayOf),
+  tracks: PropTypes.instanceOf(Array),
   addItems: PropTypes.func,
   trackDates: PropTypes.instanceOf(Array),
 };
@@ -113,4 +172,4 @@ Progress.defaultProps = {
   tracks: [],
 };
 
-export default Progress;
+export default connect(mapStateToProps, mapDispatchToProps)(Progress);
