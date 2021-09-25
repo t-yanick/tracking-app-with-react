72665c2004b565f2a57aa0463c20c1a009d25fe0
Yanick <tyankee15@gmail.com>
2021-09-25 04:42:47 -0300

    Add functions to get items, get tracks in TrackList.js

--- src/pages/TrackList.js
+++ src/pages/TrackList.js
@@ -1,12 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes, { arrayOf } from 'prop-types';
+import { moment } from '../helpers/api';
 import TrackListItem from '../components/TrackListItem';
+import { getTracks } from '../helpers/restTracks';
+import { getItems } from '../helpers/restItems';
+import { addTracks } from '../actions/tracks';
+import addItems from '../actions/items';
+import { addTrackDates } from '../actions/trackDates';
 
 const TrackList = ({
   addTracks, loginUser, addTrackDates, trackDates, tracks, items, addItems,
 }) => {
   const [error, setError] = useState('');
+
+  const runGetItems = async () => {
+    try {
+      const response = await getItems();
+      if (response.length > 0) {
+        setError('');
+        addItems(response);
+      } else {
+        setError('No Items');
+      }
+    } catch {
+      setError('Unable to fetch the item data');
+    }
+  };
+
+  const runGetTracks = async () => {
+    try {
+      const response = await getTracks();
+      if (response) {
+        setError('');
+        addTracks(response.records);
+        addTrackDates(response.record_dates);
+      } else {
+        setError('No Tracks');
+      }
+    } catch {
+      setError('Unable to fetch the data');
+    }
+  };
+
+  useEffect(() => {
+    if (loginUser) {
+      runGetItems();
+      runGetTracks();
+    }
+  }, []);
 
   return loginUser ? (
     <div className="TrackList">
@@ -34,6 +77,19 @@ const TrackList = ({
   ) : <Redirect to="/" />;
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  addTracks: (tracks) => dispatch(addTracks(tracks)),
+  addTrackDates: (trackDates) => dispatch(addTrackDates(trackDates)),
+  addItems: (items) => dispatch(addItems(items)),
+});
+
+const mapStateToProps = (state) => ({
+  items: state.items,
+  tracks: state.tracks,
+  trackDates: state.trackDates,
+  loginUser: state.user.logIn,
+});
+
 TrackList.propTypes = {
   addTracks: PropTypes.func,
   addTrackDates: PropTypes.func,
@@ -53,4 +109,4 @@ TrackList.defaultProps = {
   tracks: [],
 };
 
-export default TrackList;
+export default connect(mapStateToProps, mapDispatchToProps)(TrackList);
