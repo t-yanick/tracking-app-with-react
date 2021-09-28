762631ff2893c947f39d5d785724782bac85142e
Yanick <tyankee15@gmail.com>
2021-09-28 03:02:21 -0300

    Update TrackForm.js

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import pluralize from 'pluralize';
+import pluralize from 'pluralize';
+import { moment } from '../helpers/api';
 
 const TrackForm = ({
-  items, targetDate,
+  items, targetDate, handleSubmit, itemTitles,
 }) => {
   const [error, setError] = useState('');
-  const [date, setDate] = useState('');
-  const [state, setState] = useState('');
+  const [date, setDate] = useState(targetDate ? moment(targetDate).format('YYYY-MM-DD') : moment().format('YYYY-MM-DD'));
+  const [state, setState] = useState(itemTitles);
 
   const onDateChange = (e) => {
     setDate(e.target.value);
   }
 
-  const handleInputChange = () => {
-
+  const handleInputChange = (value, nameItemNum) => {
+    if (value.match(/^[0-9\s]{0,3}$/)) {
+      setError('');
+      setState({ ...state, [nameItemNum]: value });
+    } else {
+      setError('Please provide an integer with maximum of 3 digits');
+    }
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const formattedDate = String(moment(date).valueOf());
+    handleSubmit(formattedDate, state);
   };
 
   return (
     <div className="track-form mb3">
-      display error message here
-      <form className="">
-        <div className="">
+      {error && <p className="error-msg">{error}</p>}
+      <form className="track-form__form mb3" onSubmit={onSubmit}>
+        <div className="track-form__date">
           <input type="date" onChange={onDateChange} value={date} />
         </div>
         <div className="track-form__group mb3">
-          {/* {items.map((item) => (
+          {items.map((item) => (
             <div className="track-form__item" key={item.id}>
               <div className="track-form__icon">
                 <span className="iconify" data-icon={item.icon} data-inline="false" />
@@ -43,10 +52,10 @@ const TrackForm = ({
                 onChange={(e) => handleInputChange(e.target.value, item.id)}
                 value={state[item.id]}
               />
-              <div className="track-form__unit">plural of units</div>
-              <div className="track-form__target">target of item</div>
+              <div className="track-form__unit">{pluralize(item.unit, state[item.id])}</div>
+              <div className="track-form__target">{`/ ${item.target}`}</div>
             </div>
-          )) } */}
+          ))}
         </div>
         <button type="submit" className="btn dark">Save</button>
       </form>
@@ -54,4 +63,22 @@ const TrackForm = ({
   );
 };
 
-export default TrackForm;
+const mapStateToProps = (state) => ({
+  items: state.items,
+});
+
+TrackForm.propTypes = {
+  items: PropTypes.instanceOf(Object),
+  handleSubmit: PropTypes.func,
+  itemTitles: PropTypes.instanceOf(Object),
+  targetDate: PropTypes.number,
+};
+
+TrackForm.defaultProps = {
+  items: [],
+  handleSubmit: null,
+  itemTitles: {},
+  targetDate: 0,
+};
+
+export default connect(mapStateToProps)(TrackForm);
