5e7b9241de852c6afc1c574ab1e91c532aafad3f
Yanick <tyankee15@gmail.com>
2021-09-28 02:30:21 -0300

    Add props to signup page and to run signup authentication

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import UsersForm from '../components/UsersForm';
+import { signedUp } from '../helpers/authUsers';
+import { logIn, setUser } from '../actions/user';
 
 const SignupPage = ({
-
+  history, setUser, logIn, loginUser,
 }) => {
   const [errors, setErrors] = useState([]);
   const [msg, setMsg] = useState('');
 
-  const handleSubmit = (username, password) => {
-
+  const runSignedUpAuth = async (username, password) => {
+    try {
+      const response = await signedUp(username, password);
+      if (response.status === 'created') {
+        setMsg('You are now logging in...');
+        setErrors([]);
+        localStorage.setItem('token', response.token);
+        setUser(response.user);
+        logIn(true);
+        history.push('/tracks');
+      } else if (response.errors.length > 0) {
+        setMsg('');
+        setErrors(response.errors);
+      }
+    } catch {
+      setMsg('');
+      setErrors(['Sorry, signup was faild.']);
+    }
   };
 
-  return (
+  const handleSubmit = (username, password) => {
+    runSignedUpAuth(username, password);
+  };
+
+  return loginUser ? <Redirect to="/tracks" /> : (
     <div>
       <h1 className="heading">SignUp</h1>
       <div className="content">
@@ -26,4 +49,26 @@ const SignupPage = ({
   );
 };
 
-export default SignupPage;
+const mapStateToProps = (state) => ({
+  adminStatus: state.user.user.admin,
+  loginUser: state.user.logIn,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setUser: (user) => dispatch(setUser(user)),
+  logIn: (status) => dispatch(logIn(status)),
+});
+
+SignupPage.propTypes = {
+  history: PropTypes.instanceOf(Object),
+  logIn: PropTypes.func,
+  setUser: PropTypes.func,
+  loginUser: PropTypes.bool.isRequired,
+};
+
+SignupPage.defaultProps = {
+  history: null,
+  logIn: null,
+  setUser: null,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
