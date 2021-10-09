34402dc7d0b63e36b5ab3795180e542f1d1c4b79
Yanick <tyankee15@gmail.com>
2021-10-09 10:18:13 -0300

    Change bserURL from developmemnt to production

--- src/helpers/baseUrl.js
+++ src/helpers/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3001/api/v1';
-// const baseUrl = 'http://tracking-app-rails-api.herokuapp.com/api/v1/';
+// const baseUrl = 'http://localhost:3001/api/v1';
+const baseUrl = 'http://tracking-app-rails-api.herokuapp.com/api/v1/';
 
 export default baseUrl;
